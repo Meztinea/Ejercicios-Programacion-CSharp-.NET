c536e0677b3a39d1d57de3b72b88d1e5b3df500f
Raquel Martínez <meztinea.dev@gmail.com>
2024-12-27 18:44:06 -0600

    Agrega ejercicio 1 - Calculadora Matemática
    Calcula la edad de un usuario, el área de un círculo y la hipotenusa de un triángulo

---
+++ Ejercicio 1. Métodos - Calculadora Matemática/Calculadora Matemática/MathCalculator.cs
@@ -0,0 +1,38 @@
+﻿namespace Matematicas
+{
+    using System;
+    /// <summary>
+    /// Clase que proporciona métodos para cálculos matemáticos.
+    /// </summary>
+    public class MathCalculator
+    {
+        /// <summary>
+        /// Calcula el área de un círculo.
+        /// </summary>
+        /// <param name="radio">Radio del círculo.</param>
+        /// <returns>El área del círculo.</returns>
+        public static double CalcularAreaCirculo(double radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+        /// <summary>
+        /// Calcula la hipotenusa de un triángulo rectángulo.
+        /// </summary>
+        /// <param name="cateto1">Longitud del primer cateto.</param>
+        /// <param name="cateto2">Longitud del segundo cateto.</param>
+        /// <returns>La longitud de la hipotenusa.</returns>
+        public static double CalcularHipotenusa(double cateto1, double cateto2)
+        {
+            return Math.Sqrt(Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2));
+        }
+        /// <summary>
+        /// Calcula la edad de una persona.
+        /// </summary>
+        /// <param name="anioNacimiento">Año de nacimiento de la persona.</param>
+        /// <returns>La edad de la persona.</returns>
+        public static int CalcularEdad(int anioNacimiento)
+        {
+            return DateTime.Now.Year - anioNacimiento;
+        }
+    }
+}
---
+++ Ejercicio 1. Métodos - Calculadora Matemática/Calculadora Matemática/Program.cs
@@ -0,0 +1,36 @@
+﻿using Matematicas;
+
+namespace CalculadoraMatematica
+{
+    /// <summary>
+    /// Clase principal desde donde arranca el programa.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método principal del sistema.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Calcula el área de un círculo con datos ingresados por el usuario 
+            Console.WriteLine("Ingresa el radio del círculo en centímetros: ");
+            double radio = double.Parse(Console.ReadLine());
+            double areaCirculo = MathCalculator.CalcularAreaCirculo(radio); // llama al método estático
+            Console.WriteLine($"El área del circulo es: {areaCirculo} cm\n\n");
+
+            // Calcula la hipotenusa de un triangulo con datos ingresados por el usuario
+            Console.WriteLine("Ingresa el primer cateto del triángulo en centímetros: ");
+            double cateto1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo cateto del triángulo en centímetros: ");
+            double cateto2 = double.Parse(Console.ReadLine());
+            double hipotenusa = MathCalculator.CalcularHipotenusa(cateto1, cateto2); // llama al método estático
+            Console.WriteLine($"La hipotenusa del triángulo es: {hipotenusa} cm\n\n");
+
+            // Calcula la edad de una persona con datos ingresados por el usuario 
+            Console.WriteLine("Ingresa el año de tu nacimiento: ");
+            int anioNacimiento = int.Parse(Console.ReadLine());
+            int edad = MathCalculator.CalcularEdad(anioNacimiento); // Llama al método estático
+            Console.WriteLine($"Este año tendrás: {edad} años\n");
+        }
+    }
+}
