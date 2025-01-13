11adce316e2e738c5e2a6593379a29137fa6614c
Raquel Martínez <meztinea.dev@gmail.com>
2025-01-13 15:51:07 -0600

    Agrega Ejercicio 10 - Comparador de Genes
    Ejercicio que compara genes para dar información relevante

---
+++ Ejercicio 10. Comparador de Genes/Sistema de Comparación de Genes/Gen.cs
@@ -0,0 +1,177 @@
+﻿
+
+/// <summary>
+/// Clase Gen para crear objetos de tipo Gen
+/// </summary>
+class Gen
+{
+    private String nombre;
+    private String ubicacion;
+    private String funcion;
+    private String secuencia;
+
+
+    // Getters y Setters
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    public string Ubicacion
+    {
+        get { return ubicacion; }
+        set { ubicacion = value; }
+    }
+
+    public string Funcion
+    {
+        get { return funcion; }
+        set { funcion = value; }
+    }
+
+    public string Secuencia
+    {
+        get { return secuencia; }
+        set { secuencia = value; }
+    }
+
+    // Constructores
+    public Gen()
+    {
+    }
+
+    // Constructor con parámetros
+    public Gen(string nombre, string ubicacion, string funcion, string secuencia)
+    {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.funcion = funcion;
+        this.secuencia = secuencia;
+    }
+
+    // Destructor de objetos Gen
+    ~Gen()
+    {
+        Console.WriteLine("El objeto ha sido destruido");
+    }
+
+
+
+
+    /// <summary>
+    /// Método que permite comparar las secuencias de dos Genes
+    /// </summary>
+    /// <param name="otroGen"> El gen con el de ha de compararse el gen actual /param>
+    /// <returns> Resultado de la comparación para mostrar al usuario </returns>
+    public String CompararGenes(Gen otroGen)
+    {
+
+        // Secuencias
+        String SecEsteGen = this.secuencia;
+        String SecOtroGen = otroGen.secuencia;
+
+
+        // Dos longitudes - comparación de longitudes
+        int LongEsteGen = SecEsteGen.Length;
+        int LongOtroGen = SecOtroGen.Length;
+        bool mismaLongitud = LongEsteGen == LongOtroGen;
+        int diferencias = 0;
+        int longitudMinima = 0;
+
+        // Verificando las longitudes
+        if (mismaLongitud == false)
+        {
+            Console.WriteLine("Las secuencias tienen longitudes diferentes.");
+
+            // Cuando son diferentes longitudes calcula la longitud más pequeña para recorrer las secuencias sin errores
+            diferencias += Math.Abs(this.secuencia.Length - otroGen.secuencia.Length);
+            longitudMinima = Math.Min(this.secuencia.Length, otroGen.secuencia.Length);
+        }
+
+            
+
+        // Comparando las secuencias caracter por caracter
+        for (int i = 0; i < longitudMinima; i++)
+        {
+            if (this.secuencia[i] != otroGen.secuencia[i])
+            {
+                diferencias++;
+            }
+        }
+
+        // Mensaje de salida según las diferencias
+        if (diferencias == 0)
+        {
+            return "Las secuencias son iguales";
+        }
+        else
+        {
+            return $"Las secuencias son diferentes. Cantidad de bases diferentes: {diferencias}.";
+        }
+    }
+
+
+
+    /// <summary>
+    /// Método que permite mutar una secuencia agregando una nueva base
+    /// </summary>
+    /// <param name="nuevaBase"> Nueva base para mutar al gen </param>
+    /// <param name="posicion"> Posicion en la que debe modificarse con la nueva base </param>
+    /// <returns> Mensaje para el usuario, indica si se pudo mutar o no </returns>
+    public String MutarSecuencia(char nuevaBase, int posicion)
+    {
+        // Verificando si la posición es válida
+        if (posicion < 0 || posicion >= this.secuencia.Length)
+        {
+            return "Posición inválida, no se puede mutar la secuencia. Verifique la posición.";
+        }
+
+        // Verificando si la nueva bas es válida (A-T-C-G)
+        if (nuevaBase != 'A' || nuevaBase != 'T' || nuevaBase != 'C' || nuevaBase != 'G')
+        {
+            return "La base no es válida. Debe ser A, T, C o G";
+        }
+
+        // Utilizando un array para modificar la secuencia
+        char[] secuenciaArray = this.secuencia.ToCharArray();
+
+        // Reemplazando la base en la posición
+        secuenciaArray[posicion] = nuevaBase;
+
+        // Recuperand la secuencia en formato de String
+        this.secuencia = new string(secuenciaArray);
+
+        return $"La mutación ha sido exitosa. Esta es la nueva secuencia: {secuencia}";
+
+    }
+
+    /// <summary>
+    /// Método que permite mostrar toda la información de un Gen
+    /// </summary>
+    /// <returns> Información del gen para mostrar al usuario </returns>
+    public string MostrarInformacion()
+    {
+
+        string informacion = "\n\nInformación del Gen:\n" +
+                                "Nombre: " + this.nombre + "\n" +
+                                "Ubicación: " + this.ubicacion + "\n" +
+                                "Función: " + this.funcion + "\n" +
+                                "Secuencia: " + this.secuencia + "\n";
+
+        return informacion;
+    }
+
+    /// <summary>
+    /// Método que permite clonar a un gen generando uno nuevo e independiente 
+    /// </summary>
+    /// <returns> El nuevo gen clonado </returns>
+    public Gen ClonarGen()
+    {
+        // Creando y devolviendo una copia del GEn
+        return new Gen(this.nombre, this.ubicacion, this.funcion, this.secuencia);
+    }
+
+
+}
+
---
+++ Ejercicio 10. Comparador de Genes/Sistema de Comparación de Genes/Program.cs
@@ -0,0 +1,86 @@
+﻿
+/// <summary>
+/// Clase desde la que inicia el programa
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Método principal desde el que inicia el programa
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Gen gen1 = new Gen();
+        Gen gen2 = new Gen();
+
+
+        // Creando el Gen principal
+        Console.WriteLine("Ingresa los datos del Gen");
+        Console.WriteLine("Nombre: ");
+        gen1.Nombre = Console.ReadLine();
+        Console.WriteLine("Ubicacion: ");
+        gen1.Ubicacion = Console.ReadLine();
+        Console.WriteLine("funcion: ");
+        gen1.Funcion = Console.ReadLine();
+        Console.WriteLine("secuencia: ");
+        gen1.Secuencia = Console.ReadLine();
+
+
+        while (true)
+        {
+            // Menú de opciones 
+            Console.WriteLine("\n\nOpciones:");
+            Console.WriteLine("1. Mostrar información del Gen");
+            Console.WriteLine("2. Mutar secuencia del Gen");
+            Console.WriteLine("3. Clonar el Gen");
+            Console.WriteLine("4. Comparar dos Genes");
+            Console.WriteLine("5. Salir");
+            Console.Write("Selecciona una opción: ");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine(gen1.MostrarInformacion());
+                    break;
+
+                case "2":
+
+                    Console.WriteLine("Ingresa la nueva Base: ");
+                    char nuevaBase = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine("Ingresa la posicion: ");
+                    int posicionBase = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(gen1.MutarSecuencia(nuevaBase, posicionBase));
+                    break;
+
+                case "3":
+
+                    gen2 = gen1.ClonarGen();
+                    Console.WriteLine("Gen clonado exitosamente.");
+                    break;
+
+                case "4":
+
+                    Console.WriteLine("Ingresa la secuencia del nuevo Gen: ");
+                    String secuenciaNuevoGen = Console.ReadLine();
+                    Gen nuevoGen = new Gen();
+                    nuevoGen.Secuencia = secuenciaNuevoGen;
+                    Console.WriteLine(gen1.CompararGenes(nuevoGen));
+                    break;
+
+                case "5":
+
+                    Console.WriteLine("Saliendo del programa...");
+                    return;
+
+                default:
+
+                    Console.WriteLine("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
